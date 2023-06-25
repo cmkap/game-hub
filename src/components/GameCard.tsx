3a4ba3eb729d1fd1665fe22520b2445b8fe69e6d
chisambwe.k <chisambwe.k@carbonyte.io>
2023-06-25 11:44:49 +0100

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody } from "@chakra-ui/card";
 import { Image } from "@chakra-ui/image";
 import { Heading } from "@chakra-ui/layout";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -10,12 +11,12 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-       
-
       <Image src={game.background_image} alt="background_image" />
-     
+
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        {/* constructing an arrray of plaform objects because of api structure */}
+        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
       </CardBody>
     </Card>
   );
