37add0c1efaa4a6e32e6660963825f5814ccd6e0
chisambwe.k <chisambwe.k@carbonyte.io>
2023-06-25 16:25:24 +0100

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,26 @@
-import useGenres from "../hooks/useGenres"
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
+
 
 const GenreList = () => {
-    const { data: genres } = useGenres()
-    return (
-    <ul>
-        {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
-  )
-}
+  const { data: genres } = useGenres();
+  return (
+    <List >
+      {genres.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize='large'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
-export default GenreList
+export default GenreList;
