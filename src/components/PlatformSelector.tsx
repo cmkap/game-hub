ecf96b1a8d59f8a99d30a05d6d355c0be5f2a447
chisambwe.k <chisambwe.k@carbonyte.io>
2023-07-02 11:05:12 +0100

    Filter games by platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,18 +1,24 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
-const PlatformSelector = () => {
+interface Props {
+    onSelectPlatorm: (platform:Platform) => void
+    selectedPlatform: Platform | null
+}
+
+const PlatformSelector = ({ onSelectPlatorm, selectedPlatform }:Props) => {
   const { data: platforms, error } = usePlatforms();
   if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platforms
+        {selectedPlatform?.name || 'Platforms'}
       </MenuButton>
       <MenuList>
         {platforms.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem onClick={() => onSelectPlatorm(platform)} key={platform.id}>{platform.name}</MenuItem>
         ))}
       </MenuList>
     </Menu>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,3 @@
-
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
@@ -12,10 +11,17 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
-  metacritic: number
+  metacritic: number;
 }
 
-
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', { params : { genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 export default useGames;
