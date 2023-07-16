c75d90aa7c96077acf0e0f57f02539f661671f9b
chisambwe.k <chisambwe.k@carbonyte.io>
2023-07-16 20:09:57 +0100

    Refactoring - remove duplicate interfaces

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,24 +1,25 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatforms";
-import { Platform } from "../hooks/useGames";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
-    onSelectPlatorm: (platform:Platform) => void
-    selectedPlatform: Platform | null
+  onSelectPlatorm: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({ onSelectPlatorm, selectedPlatform }:Props) => {
+const PlatformSelector = ({ onSelectPlatorm, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
   if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || 'Platforms'}
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data?.results.map((platform) => (
-          <MenuItem onClick={() => onSelectPlatorm(platform)} key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem onClick={() => onSelectPlatorm(platform)} key={platform.id}>
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
