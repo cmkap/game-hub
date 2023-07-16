f5420790387b47ac2a1b4a5558d0809d819fa6f4
chisambwe.k <chisambwe.k@carbonyte.io>
2023-07-16 18:23:07 +0100

    Update usePlatoforms and Add Platforms to ship with application

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,10 @@
-import useData from "./useData";
-import { Platform } from "./useGames";
+// import useData from "./useData";
+// import { Platform } from "./useGames";
+import platforms from "../data/platforms";
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+
+// const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null})
 
 export default usePlatforms
