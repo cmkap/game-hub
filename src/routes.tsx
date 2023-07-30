033eb75ec3a2ec066b3a2974016169eae2c12baa
chisambwe.k <chisambwe.k@carbonyte.io>
2023-07-30 17:20:10 +0100

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,10 +3,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetailsPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage />,
     element: <Layout />,
     children: [
         { index: true, element: <HomePage />},
