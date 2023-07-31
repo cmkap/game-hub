5fccf5a7156451d3fb010f979079184179168080
chisambwe.k <chisambwe.k@carbonyte.io>
2023-07-31 17:33:42 +0100

    Refactor- move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,16 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import ms from 'ms';
+import ms from "ms";
 
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
-const apiClient = new APIClient<Genre>('/genres')
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
+const apiClient = new APIClient<Genre>("/genres");
 
 // const useGenres = () => ({data: genres, isLoading: false, error: null});
 
@@ -18,8 +13,8 @@ const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: ms('24h'),
-    initialData: genres, 
+    staleTime: ms("24h"),
+    initialData: genres,
   });
 };
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,20 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import ms from 'ms'
+import ms from "ms";
 
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
+import { Platform } from "../entities/Platform";
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: ms('24h'), // 24hours
+    staleTime: ms("24h"), // 24hours
     initialData: platforms,
   });
 
