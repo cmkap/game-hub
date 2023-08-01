13eb3f9f7d56f7979ef4576438c4b6ba3c7f2830
chisambwe.k <chisambwe.k@carbonyte.io>
2023-08-01 15:12:44 +0100

    Update Navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,16 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 const NavBar = () => {
+  const navigate = useNavigate()
   return (
     <HStack padding='10px'>
-      <Image src={logo} boxSize='60px'/>
+      <Image src={logo} boxSize='60px' onClick={() => navigate('/')}/>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
